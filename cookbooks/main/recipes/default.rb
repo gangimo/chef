5cc8310f7b51e9994eecd0d19b620ccd22559404
gangimo <gangimo@gmail.com>
2014-01-01 22:45:47 +0900

    tomcat7追加

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,3 +3,6 @@ include_recipe "main::update"
 
 #java7(openjdk)
 include_recipe "java7::setup"
+
+#tomcat7
+include_recipe "tomcat7::setup"
---
+++ cookbooks/tomcat7/attributes/setup.rb
@@ -0,0 +1,16 @@
+
+default[:tomcat7][:version] = "7.0.47"
+default[:tomcat7][:user] = "tomcat"
+default[:tomcat7][:group] = "tomcat"
+default[:tomcat7][:target] = "/usr/share"
+default[:tomcat7][:port] = 8080
+default[:tomcat7][:ssl_port] = 8443
+default[:tomcat7][:ajp_port] = 8009
+default[:tomcat7][:java_options] = " -Xmx128M -Dajva.awt.headless=true"
+default[:tomcat7][:use_security_manager] = "no"
+
+##
+set[:tomcat7][:home] = "#{tomcat7['target']}/tomcat"
+set[:tomcat7][:base] = "#{tomcat7['target']}/tomcat"
+set[:tomcat7][:config_dir] = "#{tomcat7['target']}/tomcat/conf"
+set[:tomcat7][:log_dir] = "#{tomcat7['target']}/tomcat/logs"
